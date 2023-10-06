54f362af1868f7ce6f80e9f203fac52c55b0566f
mcodreanu <mihai.alexxandru03@gmail.com>
2023-10-06 17:03:01 +0200

    commit

--- DWS/html/UD2/Ejercicios/Calculadora/calculadora_usuario.php
+++ DWS/html/UD2/Ejercicios/Calculadora/calculadora_usuario.php
@@ -3,12 +3,12 @@ ini_set('display_errors', 'On');
 ini_set('html_errors', 0);
 
 require ("calculadora.php");
-function test_factorial_1()
+function test_factorial()
 {
     $calculadora = new Calculadora();
-    $x = $calculadora->factorial(0);
+    $x = $calculadora->factorial(5);
 
-    return ($x == 1);
+    return $x;
 }
 
 function test_coeficienteBinomial()
@@ -40,16 +40,11 @@ function test_esPalindromo()
     $calculadora = new Calculadora();
     $x = $calculadora->esPalindromo("Isaac no ronca asi");
 
-    if ($x)
-    {
-        echo "True";
-    }
-    else
-    {
-        echo "False";
-    }
-
     return $x;
 }
 
-echo test_esPalindromo();
+echo "Test factorial de 5: " . test_factorial();
+echo "<br>Test coeficiente binomial de n = 9 y k = 3: " . test_coeficienteBinomial();
+echo "<br>Test conversión de binario a decimal de \"1101100\": " . test_binarioDecimal();
+echo "<br>Test suma numeros pares de [2,3,6,8,9,10,23,24]: " . test_sumaNumerosPares();
+echo "<br>Test palindromo de \"Isaac no ronca asi\": " . test_esPalindromo();
