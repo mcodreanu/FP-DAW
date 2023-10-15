0ba655952dbeaf02c31a3d9297f0c3b25ae1674b
mcodreanu <mihai.alexxandru03@gmail.com>
2023-10-15 19:24:39 +0200

    commit

--- DWS/U2/Ejercicios/Ejer1_2_3/Program.cs
+++ DWS/U2/Ejercicios/Ejer1_2_3/Program.cs
@@ -6,37 +6,81 @@ namespace Ejercicios
     {
         public static bool Ejer01 (int[,] numeros)
         {
-            int suma1 = 0;
-            int suma2 = 0;
-            int suma3 = 0;
+            int sumaFila = 0;
 
-            for (int fila = 0; fila < numeros.GetLength(1); fila++)
+            for (int i = 0; i < numeros.GetLength(1); i++)
             {
-                suma1 = suma1 + numeros[0,fila];
-                suma2 = suma2 + numeros[1,fila];
-                suma3 = suma3 + numeros[2,fila];
+                sumaFila += numeros[0, i];
             }
 
-            if (suma1 == suma2 && suma2 == suma3)
+            for (int i = 1; i < numeros.GetLength(0); i++)
             {
-                return true;
+                int sumaFilaActual = 0;
+
+                for (int j = 0; j < numeros.GetLength(1); j++)
+                {
+                    sumaFilaActual += numeros[i, j];
+                }
+
+                if (sumaFilaActual != sumaFila)
+                {
+                    return false;
+                }
             }
-            else
+
+            return true;
+        }
+
+        public static int Ejer02 (int[,] numeros)
+        {
+            int indiceMayorSuma = 0;
+            int mayorSuma = 0;
+
+            for (int i = 0; i < numeros.GetLength(0); i++)
+            {
+                int sumaFila = 0;
+                for (int j = 0; j < numeros.GetLength(1); j++)
+                {
+                    sumaFila += numeros[i, j];
+                }
+
+                if (sumaFila > mayorSuma)
+                {
+                    mayorSuma = sumaFila;
+                    indiceMayorSuma = i;
+                }
+            }
+
+            return indiceMayorSuma;
+    }
+
+        public static bool Ejer03 (int[,] numeros, List<int> enteros)
+        {
+        foreach (int numero in numeros)
+        {
+            if (!enteros.Contains(numero))
             {
                 return false;
             }
         }
 
+        return true;
+    }
+
         static void Main(string[] args)
         {
-            int[,] numeros = new int[3,3]
+            int[,] numeros = new int[,]
             {
                 {1,2,3},
-                {3,2,1},
-                {3,1,2}
+                {4,5,6},
+                {7,8,9}
             };
 
-            Console.WriteLine(Ejer01(numeros));
+            List<int> enteros = new List<int> {1,3,5,7,9};
+
+            Console.WriteLine("Ejercicio 1: " + Ejer01(numeros));
+            Console.WriteLine("Ejercicio 2: " + Ejer02(numeros));
+            Console.WriteLine("Ejercicio 3: " + Ejer03(numeros, enteros));
         }
     }
 }
