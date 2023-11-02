cb312dc9bfc911749a05545a768daca826e12796
mcodreanu <mihai.alexxandru03@gmail.com>
2023-11-02 16:51:24 +0100

    commit

--- DWC/EjerciciosUD2/js/script.js
+++ DWC/EjerciciosUD2/js/script.js
@@ -12,3 +12,36 @@ function cerrarSobre(){
     img.src = 'img/sobre-cerrado.png';
     img.setAttribute("onclick", "abrirSobre()"); 
 }
+
+//Ejercicio 02
+function inInterval() {
+    
+    let num = document.getElementById("num").value;
+    
+    let res;
+
+    if (isNaN(num) || num < -1 || num > 1) {
+      res = "Valor erroni";
+    } else {
+      res = "Valor correcte";
+    }
+
+    document.getElementById("text").innerHTML = res;
+}
+
+//Ejercicio 03
+function calcularCoeficiente() {
+    
+    let valor1 = document.getElementById("valor1").value;
+    let valor2 = document.getElementById("valor2").value;
+    
+    let x;
+
+    if (isNaN(valor1) || isNaN(valor2)) {
+      x = "Valor erroni";
+    } else {
+      x = -valor2/valor1;
+    }
+
+    document.getElementById("res").innerHTML = x;
+}
