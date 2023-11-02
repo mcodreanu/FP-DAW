025803dc06a1285e819e84a73661d10dc8f1b45b
mcodreanu <mihai.alexxandru03@gmail.com>
2023-11-02 18:56:24 +0100

    commit

--- DWC/EjerciciosUD2/js/script.js
+++ DWC/EjerciciosUD2/js/script.js
@@ -45,3 +45,29 @@ function calcularCoeficiente() {
 
     document.getElementById("res").innerHTML = x;
 }
+
+//Ejercicio 04
+function calcularAreaYLongitud() {
+    
+    let radi = document.getElementById("radi").value;
+    
+    let area = Math.PI * Math.pow(radi, 2);
+    let longitud = 2 * Math.PI * radi;
+
+    if (isNaN(radi)) {
+        document.getElementById("area").innerHTML = "VALOR ERRONI";
+        document.getElementById("longitud").innerHTML = "VALOR ERRONI";
+    } else {
+        document.getElementById("area").innerHTML = area.toFixed(4);
+        document.getElementById("longitud").innerHTML = longitud.toFixed(4);
+    }
+}
+
+//Ejercicio 05
+window.onload = function cambiarColor() {
+    
+    let paragraf = document.getElementById("paragraf");
+
+    paragraf.onmouseover = function() {paragraf.style.color = "blue";}  
+    paragraf.onmouseout = function() {paragraf.style.color = "red";}  
+}
