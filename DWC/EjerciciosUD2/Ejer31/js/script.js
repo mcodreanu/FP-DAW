52fa502cbada1f9cb1f36b4e06bd78ab32082da7
mcodreanu <mihai.alexxandru03@gmail.com>
2023-11-22 16:06:15 +0100

    commit

--- DWC/EjerciciosUD2/Ejer31/js/script.js
+++ DWC/EjerciciosUD2/Ejer31/js/script.js
@@ -1,3 +1,17 @@
-window.onload = function juegoDau(){
-      
+window.onload = function juegoDado(){
+    let img1 = document.getElementById("img-dado1");
+    let img2 = document.getElementById("img-dado2");
+    let numRandom1 = Math.floor((Math.random() * (6 - 1 + 1) + 1));
+    let numRandom2 = Math.floor((Math.random() * (6 - 1 + 1) + 1));
+
+    document.getElementById("img-dado1").src = "img/" + numRandom1 + ".svg";
+    document.getElementById("img-dado2").src = "img/" + numRandom2 + ".svg";
+
+    if (numRandom1 > numRandom2) {
+        document.getElementById("ganador").innerHTML = "Ha guanyat el jugador 1";
+    } else if (numRandom2 > numRandom1) {
+        document.getElementById("ganador").innerHTML = "Ha guanyat el jugador 2";
+    } else {
+        document.getElementById("ganador").innerHTML = "Empate";
+    }
 }
